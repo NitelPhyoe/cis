29c447d3dbbb0e317f5505627b2a9c512ae2e274
NitelPhyoe <moepyae2262@gmail.com>
2023-11-12 22:26:14 +0900

    add shadcn && prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { prisma } from "@orm/client";
 
-export default function Home() {
+async function getUser() {
+  "use server";
+  return await prisma.user.findMany();
+}
+
+export default async function Home() {
+  const data = await getUser();
   return (
     <main>
+      {JSON.stringify(data)}
       <Button>Click me</Button>
       <Badge variant="outline">Badge</Badge>
     </main>
